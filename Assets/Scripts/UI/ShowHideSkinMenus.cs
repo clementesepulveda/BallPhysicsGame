f185f8d61c3a943931db552beeff8b661c5ea707
clementesepulveda <clemente.seplveda@uc.cl>
2022-12-30 12:31:41 -0300

    ball assets

--- Assets/Scripts/UI/ShowHideSkinMenus.cs
+++ Assets/Scripts/UI/ShowHideSkinMenus.cs
@@ -1,12 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class ShowHideSkinMenus : MonoBehaviour
+
+public class ShowHideSkinMenus : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public int buttonId;
     public Image buttonImage;
     public GameObject[] menus;
+
+    public float onHoverDownTranslate = -10;
+    private Vector3 originalPosition;
+
+    void Start() {
+        originalPosition = transform.position;
+    }
 
     public void SetThisMenu() {
         foreach (var menu in menus) {
@@ -14,5 +24,15 @@ public class ShowHideSkinMenus : MonoBehaviour
         }
 
         menus[buttonId].SetActive(true);
+    }
+     
+    public void OnPointerEnter(PointerEventData eventData) {
+        transform.position += new Vector3(0, onHoverDownTranslate, 0);
+        Debug.Log("Enter");
+    }
+
+    public void OnPointerExit(PointerEventData eventData) {
+        transform.position -= new Vector3(0, onHoverDownTranslate, 0);
+        Debug.Log("Exit");
     }
 }
